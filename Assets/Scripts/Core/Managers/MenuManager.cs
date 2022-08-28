fd5b980d7244fa546889e94f222b00c5f4d60109
Ian Geier <ianallengeier@gmail.com>
2022-08-28 20:32:17 -0300

    create server button

--- Assets/Scripts/Core/Managers/MenuManager.cs
+++ Assets/Scripts/Core/Managers/MenuManager.cs
@@ -1,5 +1,6 @@
 ﻿using Photon.Pun;
 using Photon.Realtime;
+using Photon.Voice.PUN;
 using ProjectNet.ScriptableObjects.Game;
 using TMPro;
 using UnityEngine;
@@ -16,6 +17,7 @@ namespace ProjectNet.Core.Managers
 
 		[Header("UI")]
 		[SerializeField] private Button loginButton;
+		[SerializeField] private Button createServerButton;
 		[SerializeField] private TMP_InputField screenNameInputField, roomNameInputField;
 
 		#endregion
@@ -24,15 +26,20 @@ namespace ProjectNet.Core.Managers
 		private void Start()
 		{
 			loginButton.interactable = false;
+			createServerButton.interactable = false;
 			PhotonNetwork.ConnectUsingSettings();
 
 			loginButton.onClick.AddListener(OnLoginButtonClick);
+			createServerButton.onClick.AddListener(CreateServer);
 		}
 
 		public override void OnConnectedToMaster() => PhotonNetwork.JoinLobby();
-		public override void OnJoinedLobby() => loginButton.interactable = true;
+		public override void OnJoinedLobby() {
+			loginButton.interactable = true;
+			createServerButton.interactable = true;
+		}
 
-		public void OnLoginButtonClick()
+		private void OnLoginButtonClick()
 		{
 			//trim input fields
 			screenNameInputField.text = screenNameInputField.text.Trim();
@@ -53,7 +60,7 @@ namespace ProjectNet.Core.Managers
 			}
 
 			PhotonNetwork.NickName = screenNameInputField.text;
-			
+
 			var maxPlayer = gameSettings.maxPlayers + 1;
 			var roomOptions = new RoomOptions{
 				MaxPlayers = (byte) maxPlayer,
@@ -63,9 +70,18 @@ namespace ProjectNet.Core.Managers
 
 			PhotonNetwork.JoinOrCreateRoom(roomNameInputField.text, roomOptions, TypedLobby.Default);
 			loginButton.interactable = false;
+			createServerButton.interactable = false;
 		}
 
-		public override void OnJoinedRoom() 
+		private void CreateServer()
+		{
+			screenNameInputField.text = "Server";
+			roomNameInputField.text = Random.Range(100, 300).ToString();
+			FindObjectOfType<PhotonVoiceNetwork>().PrimaryRecorder = null;
+			OnLoginButtonClick();
+		}
+
+		public override void OnJoinedRoom()
 		{
 			PhotonNetwork.AutomaticallySyncScene = true;
 			if (PhotonNetwork.IsMasterClient)
@@ -74,16 +90,18 @@ namespace ProjectNet.Core.Managers
 			}
 		}
 
-        public override void OnCreateRoomFailed(short returnCode, string message)
+		public override void OnCreateRoomFailed(short returnCode, string message)
 		{
 			Debug.LogError($"Failed to create room. Error code: {returnCode} Message: {message}");
 			loginButton.interactable = true;
+			createServerButton.interactable = true;
 		}
 
 		public override void OnJoinRoomFailed(short returnCode, string message)
 		{
 			Debug.LogError($"Failed to join room. Error code: {returnCode} Message: {message}");
 			loginButton.interactable = true;
+			createServerButton.interactable = true;
 		}
 	}
 }
