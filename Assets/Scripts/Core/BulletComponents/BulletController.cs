b6ecbbcc08d6e6446fc0071124e9e48ae08c9cfe
Ian Geier <ianallengeier@gmail.com>
2022-08-29 20:37:14 -0300

    update

--- Assets/Scripts/Core/BulletComponents/BulletController.cs
+++ Assets/Scripts/Core/BulletComponents/BulletController.cs
@@ -38,5 +38,11 @@ namespace ProjectNet.Core.BulletComponents
 				PhotonNetwork.Destroy(gameObject);
 			}
 		}
+
+
+		private void OnTriggerEnter2D(Collider2D col)
+		{
+			PhotonNetwork.Destroy(gameObject);
+		}
 	}
 }
